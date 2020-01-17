c3dadecdba9e739d87459df40940350a3130f71e
Saurabh.Bhushan <Saurabh.Bhushan@diasparkindia.com>
2020-01-17 21:53:51 +0530

    update

--- Ratno_Tech/Controllers/HomeController.cs
+++ Ratno_Tech/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Web;
 using System.Web.Mvc;
 
@@ -25,6 +27,34 @@ namespace Ratno_Tech.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Contact(Ratno_Tech.MailModel _objModelMail)
+        {
+            if (ModelState.IsValid)
+            {
+                MailMessage mail = new MailMessage();
+                mail.To.Add(_objModelMail.To);
+                mail.From = new MailAddress(_objModelMail.From);
+                mail.Subject = _objModelMail.Subject;
+                string Body = _objModelMail.Body;
+                mail.Body = Body;
+                mail.IsBodyHtml = true;
+                SmtpClient smtp = new SmtpClient();
+                smtp.Host = "smtp.gmail.com";
+                smtp.Port = 587;
+                smtp.UseDefaultCredentials = false;
+                smtp.Credentials = new System.Net.NetworkCredential("saurabhbhushan.sb@gmail.com", "*7070889825#"); // Enter seders User name and password   
+                smtp.EnableSsl = true;
+                smtp.Send(mail);
+                return View("Contact", _objModelMail);
+            }
+            else
+            {
+                return View();
+            }
         }
     }
 }
